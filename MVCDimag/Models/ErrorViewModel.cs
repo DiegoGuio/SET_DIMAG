0e84cc4004ad73bd56aa501ee803c8398fc61130
Diego Guio <diegofernandoguio@gmail.com>
2024-08-02 19:51:12 -0500

    Se crea el proyecto MVC para el maquetado del Frontend del Proyecto

---
+++ MVCDimag/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCDimag.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- WebApiDIMAG/obj/Debug/net8.0/WebApiDIMAG.AssemblyInfo.cs
+++ WebApiDIMAG/obj/Debug/net8.0/WebApiDIMAG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiDIMAG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a58f090417a49a8b646681d78a4301100dd2ebc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f260daec8fb8d53505171b566d2a383556bc823b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiDIMAG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiDIMAG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
