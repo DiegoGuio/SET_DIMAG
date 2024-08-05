9b1377ea2a8b64ff3fa26e9334edd95bfbc7ea44
Diego Guio <diegofernandoguio@gmail.com>
2024-08-05 07:15:13 -0500

    Se agregan los comentarios a las funciones del controlador de usuarios del WebApiDimag

--- WebApiDIMAG/Controllers/UsuariosController.cs
+++ WebApiDIMAG/Controllers/UsuariosController.cs
@@ -13,31 +13,50 @@ namespace WebApiDIMAG.Controllers
         {
             _bL = bL;
         }
-
+        /// <summary>
+        /// Fncion para registar los usuarios nuevo
+        /// </summary>
+        /// <param name="usuarioDto"></param>
+        /// <returns>1 Exitoso, 2 Usuario ya existe, 3 Fallido</returns>
         [HttpPost("RegistrarUsuario")]
         public int RegistrarUsuario([FromBody] UsuarioDto usuarioDto)
         {
             return _bL.RegistrarUsuario(usuarioDto);
         }
-
+        /// <summary>
+        /// Consultar informaciòn con el nombre de usuario
+        /// </summary>
+        /// <param name="nombreUsuario"></param>
+        /// <returns>Informaciòn del usuairo consultado</returns>
         [HttpGet("ConsultarUsuarioPorNombreUsuario")]
         public UsuarioDto ConsultarUsuarioPorNombreUsuario(string nombreUsuario)
         {
             return _bL.ConsultarUsuarioPorNombreUsuario(nombreUsuario);
         }
-
+        /// <summary>
+        /// Eliminación individual de usuarios
+        /// </summary>
+        /// <param name="nombreUsuario"></param>
+        /// <returns>True existosa, false fallida</returns>
         [HttpDelete("EliminarUsuarioPorNombreUsuario")]
         public bool EliminarUsuarioPorNombreUsuario(string nombreUsuario)
         {
             return _bL.EliminarUsuarioPorNombreUsuario(nombreUsuario);
         }
-
+        /// <summary>
+        /// Se obtiene el listado de generos disponibles
+        /// </summary>
+        /// <returns>Listado de generos con Id y nombre</returns>
         [HttpGet("ObtenerGeneros")]
         public List<GeneroDto> ObtenerGeneros()
         {
             return _bL.ObtenerGeneros();
         }
-
+        /// <summary>
+        /// Registra las medidas corporales de los usuarios y establece la talla adecuada
+        /// </summary>
+        /// <param name="data">Son las medidas corporales ingresadas por el usuario</param>
+        /// <returns>Retorna los datos ingresados por el usuario, agrgandole la talla determinada y la fecha de  registro. Esta información se almacena en la base  de datos</returns>
         [HttpPost("RegistroMedidasCorporalesPorUsuario")]
         public RegistroMedidasCorporalesPorUsuarioDto RegistroMedidasCorporalesPorUsuario(RegistroMedidasCorporalesPorUsuarioDto data)
         {
